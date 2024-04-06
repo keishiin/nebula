f1462084faa33f681d03468ec78d7220cc8a4f2f
root <root@keishin>
2024-04-05 23:37:03 -0400

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,82 @@
-use std::env;
 use dotenv::dotenv;
 use poise::serenity_prelude as serenity;
+use serenity::FullEvent;
+
+// mod misc_commands;
 
 struct Data {}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
+const _POKEURL: &str = "https://pokeapi.co/api/v2/pokemon/";
+
 #[poise::command(slash_command, prefix_command)]
-async fn age(
+pub async fn avatar(
+    ctx: Context<'_>,
+    #[description = "users avatar"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+
+    let embed = serenity::CreateEmbed::new()
+        .title(format!("@{}'s avatar", u.name))
+        .image(u.face())
+        .color(serenity::Colour::BLUE);
+
+    ctx.send(poise::CreateReply::default().embed(embed)).await?;
+
+    Ok(())
+}
+
+#[poise::command(slash_command, prefix_command)]
+pub async fn age(
     ctx: Context<'_>,
     #[description = "Selected user"] user: Option<serenity::User>,
 ) -> Result<(), Error> {
     let u = user.as_ref().unwrap_or_else(|| ctx.author());
-    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+
+    let response = format!("@{}'s account was created at {}", u.name, u.created_at());
     ctx.say(response).await?;
+
+    Ok(())
+}
+
+/// Show this help menu
+#[poise::command(prefix_command, track_edits, slash_command)]
+pub async fn help(
+    ctx: Context<'_>,
+    #[description = "Specific command to show help about"]
+    #[autocomplete = "poise::builtins::autocomplete_command"]
+    command: Option<String>,
+) -> Result<(), Error> {
+    poise::builtins::help(
+        ctx,
+        command.as_deref(),
+        poise::builtins::HelpConfiguration {
+            extra_text_at_bottom: "This is an example bot made to showcase features of my custom Discord bot framework",
+            ..Default::default()
+        },
+    )
+    .await?;
     Ok(())
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     dotenv().ok();
     let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
     let intents = serenity::GatewayIntents::non_privileged();
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![age()],
+            commands: vec![age(), help(), avatar()],
+            event_handler: |ctx, event, framework, data| {
+                Box::pin(event_handler(ctx, event, framework, data))
+            },
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: Some("!".into()),
+                case_insensitive_commands: true,
+                ..Default::default()
+            },
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
@@ -40,4 +91,26 @@ async fn main() {
         .framework(framework)
         .await;
     client.unwrap().start().await.unwrap();
+
+    Ok(())
+}
+
+async fn event_handler(
+    _ctx: &serenity::Context,
+    event: &serenity::FullEvent,
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+    _data: &Data,
+) -> Result<(), Error> {
+    match event {
+        FullEvent::Ready { data_about_bot, .. } => {
+            println!(
+                "Ready! Logged in as {}#{}",
+                data_about_bot.user.name,
+                // Should never be None, as bots still use the "Name#0000" format instead of usernames
+                data_about_bot.user.discriminator.unwrap()
+            );
+        }
+        _ => {}
+    }
+    Ok(())
 }
